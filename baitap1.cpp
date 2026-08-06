c9f441be2b9f5330f00615970435958ff5fbc8fe
raymenhoanganh-ctrl <raymen.hoanganh@gmail.com>
2026-08-06 09:41:07 +0700

    uploard source code

---
+++ baitap1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "---------------------------" << endl;
+    cout << "Ho ten: Hoang Anh Giap" << endl;
+    cout << "Lop: C++ K01" << endl;
+    cout << "So thich: Choi game" << endl;
+    cout << "Muc tieu: Thanh thao lap trinh C++ sau 6 thang" << endl;
+    cout << "---------------------------" << endl;
+    return 0;
+}
